a44f24e1165cacd73a02d234bfd99e7a3ec68106
calebewerneckcouto <calebewerneck@hotmail.com>
2024-07-22 09:53:28 -0300

    array

---
+++ array.js
@@ -0,0 +1,9 @@
+let frutas = ["Maçã","Banana","Limão","Pêra","Abacaxi"];
+
+let pessoa = {nome : "Calebe", idade :36}
+
+console.log(frutas[2]);
+console.log(frutas);
+console.log(pessoa.nome);
+
+console.log(frutas.length)
---
+++ arrayexercicios.js
@@ -0,0 +1,18 @@
+let cores =[ "Vermelho", "Verde", "Azul", "Amarelo"]
+
+console.log(cores[3])
+
+cores.push("Dourado")//adicionado ultimo item do array.....
+
+console.log(cores)
+
+let ultimaCor = cores.pop();//exclui a ultima cor
+
+console.log(cores)
+
+cores.unshift("Dourado")//adiciona no comeco do array
+
+console.log(cores)
+
+
+
